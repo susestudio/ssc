022831078a9cbfa92f889395427bc0c98dd79920
Ratan Sebastian <rjsvaljean@gmail.com>
2011-06-03 12:13:32 +0530

    added repository searching. `ssc repository search <package name> [--base_system <system name>]` now works

--- lib/handlers/repository.rb
+++ lib/handlers/repository.rb
@@ -1,3 +1,23 @@
-class SSC::Handler::Repository
-  
+module SSC
+  module Handler
+    class Repository
+      
+      include Helper
+
+      def initialize(options = {})
+        @options= options
+        connect(@options[:username], @options[:password], options)
+      end
+
+      def search(search_string)
+        params= {:filter => search_string}
+        params= params.merge({:base_system => @options[:base_system]}) if @options[:base_system]
+        repos= StudioApi::Repository.find(:all, :params => params)
+        repos.collect do |repo|
+          "#{repo.name}: #{repo.base_url}
+          #{[repo.matches.software_name].flatten.join(', ')}\n"
+        end
+      end
+    end
+  end
 end
