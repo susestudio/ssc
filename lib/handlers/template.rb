809b770802bcebe9ab8e518eb0a3e138d66ed4b4
Cristian Mircea Messel <mess110@gmail.com>
2011-07-19 15:15:08 +0200

    fix template method names
    list will now display the template names within a template set.
    syntax to view template sets is list_sets instead of list

--- lib/handlers/template.rb
+++ lib/handlers/template.rb
@@ -2,16 +2,16 @@ module SSC
   module Handler
     class Template < Base
 
-      desc 'template list', 'list all available template sets'
+      desc 'template list_sets', 'list all available template sets'
       require_authorization
-      def list
+      def list_sets
         templates= StudioApi::TemplateSet.find(:all)
         say_array templates.collect {|template| template.name}
       end
 
-      desc 'template show NAME', 'show details of a particular template set'
+      desc 'template list SET_NAME', 'show details of a particular template set'
       require_authorization
-      def show(name)
+      def list(name)
         template_set= StudioApi::TemplateSet.find(name)
         out = [template_set.name+' : '+template_set.description]
         out += template_set.template.collect do |appliance| 
