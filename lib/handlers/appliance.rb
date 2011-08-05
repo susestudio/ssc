86081316c47d3ebbfbd337fbfda8a397967e670a
Ratan Sebastian <rjsvaljean@gmail.com>
2011-08-05 11:56:13 +0530

    moved build and build status to appliance handler

--- lib/handlers/appliance.rb
+++ lib/handlers/appliance.rb
@@ -64,6 +64,38 @@ module SSC
         end
       end
 
+      desc "build", "build an appliance"
+      require_appliance_id
+      method_option :image_type, :type => :string, :required => true
+      def build
+        require_appliance do |appliance|
+          if appliance.status.state != "ok"
+            raise Thor::Error, "Appliance is not OK. Please fix before building.\n#{appliance.status.issues.join("\n")}\n"
+          else
+            build = StudioApi::RunningBuild.new(:appliance_id => appliance.id, :image_type => options.image_type)
+            build.save
+            config_file= File.join(Dir.pwd, '.sscrc')
+            if File.exists?(config_file)
+              config= YAML::load(File.read(config_file))
+              config.merge!('latest_build_id' => build.id)
+              File.open(config_file, 'w') do |file|
+                file.write(config.to_yaml)
+              end
+            end
+            say "Build Started. Build id: #{build.id}"
+          end
+        end
+      end
+
+      desc "build_status", "find the build status of an appliance"
+      require_authorization
+      require_build_id
+      def build_status
+        build = StudioApi::Build.find options.build_id
+        additional_info=(build.state == 'finished' ? "" : " - #{build.percent}")
+        say "Build Status: #{build.state}" + additional_info
+      end
+
       private
 
       def download_url(appliance)
