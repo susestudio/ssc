3ede83ca43cd60316cf549de3979cb42a3800b6c
Cristian Mircea Messel <cmessel@suse.de>
2011-08-10 14:17:17 +0200

    make ssc help text more readable
    Indentation greated than 80 columns made it difficult to read in most
    terminals.

--- lib/ssc.rb
+++ lib/ssc.rb
@@ -121,18 +121,18 @@ module SSC
       def help(*args)
         message= <<HELP_MESSAGE 
 Tasks:
-  ssc checkout --appliance-id=N --password=PASSWORD --username=USERNAME  # checkout the latest changes to an appliance
-  ssc commit --appliance-id=N --password=PASSWORD --username=USERNAME    # commit changes to studio
-  ssc status --appliance-id=N --password=PASSWORD --username=USERNAME    # show status of the appliance
+  ssc checkout          # checkout the latest changes to an appliance
+  ssc commit            # commit changes to studio
+  ssc status            # show status of the appliance
 
-  ssc appliance                                                          # manage appliances
-  ssc build                                                              # manage builds
-  ssc file                                                               # manage files
-  ssc package                                                            # manage packages
-  ssc repository                                                         # manage repositories
-  ssc template                                                           # manage templates
+  ssc appliance         # manage appliances
+  ssc build             # manage builds
+  ssc file              # manage files
+  ssc package           # manage packages
+  ssc repository        # manage repositories
+  ssc template          # manage templates
 
-  ssc help [TASK]                                                        # Describe available tasks or one specific task
+  ssc help [TASK]       # Describe available tasks or one specific task
 HELP_MESSAGE
        puts message
       end
