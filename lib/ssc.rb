f2d0395a1246d98ffade1656ccffdc910460dca5
Cristian Mircea Messel <mess110@gmail.com>
2011-09-07 17:21:20 +0200

    add repos before adding packages when commiting

--- lib/ssc.rb
+++ lib/ssc.rb
@@ -86,14 +86,6 @@ module SSC
                :appliance_id => options.appliance_id,
                :username     => options.username,
                :password     => options.password}
-      # Add, Remove, Ban and Unban  Packages
-      package_file= PackageFile.new
-      ["add", "remove", "ban", "unban"].each do |action|
-        while package= package_file.pop(action)
-          invoke "s_s_c:handler:package:#{action}", [package], params
-        end
-      end
-      package_file.save
 
       # Add or Remove Repositories
       repository_file= RepositoryFile.new
@@ -103,6 +95,15 @@ module SSC
         end
       end
       repository_file.save
+
+      # Add, Remove, Ban and Unban  Packages
+      package_file= PackageFile.new
+      ["add", "remove", "ban", "unban"].each do |action|
+        while package= package_file.pop(action)
+          invoke "s_s_c:handler:package:#{action}", [package], params
+        end
+      end
+      package_file.save
 
       # Add Overlay Files
       file_list = FileListFile.new
