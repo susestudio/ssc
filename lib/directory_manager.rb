1745858099c388cd2bebe9f0f38adbc4b45577cb
vlewin <vlewin@gmx.de>
2012-06-15 14:42:03 +0200

    function 'clean' breaks test cases, this function create files 'software' and 'repository' in root directory

--- lib/directory_manager.rb
+++ lib/directory_manager.rb
@@ -35,7 +35,6 @@ module SSC
       end
       
       def push(section, item)
-        clean
         read
         if @parsed_file[section].is_a?(Array)
           @parsed_file[section] |= [ item ]
@@ -56,10 +55,6 @@ module SSC
       def empty_list?
         read
         (!@parsed_file['list']) || (@parsed_file['list'] == []) || (@parsed_file['list'] == {})
-      end
-
-      def clean
-        File.open(@location, 'w') {}
       end
     end
 
